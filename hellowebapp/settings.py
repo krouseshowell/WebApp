06db7df79198013394e0d34308db8c110af4065d
kendrarose <kendrarose@grdylan.net>
2017-05-05 13:16:23 -0700

    moved static

--- hellowebapp/settings.py
+++ hellowebapp/settings.py
@@ -121,10 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_Root = 'staticfiles'
+STATIC_ROOT = 'staticfiles'
 STATIC_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-)
+    )
+
 ACCOUNT_ACTIVATION_DAYS = 7
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
