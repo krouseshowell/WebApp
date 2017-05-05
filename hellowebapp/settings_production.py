4e89398187a92ed574f801df643786e503793488
kendrarose <kendrarose@grdylan.net>
2017-05-05 12:37:00 -0700

    Added production settings file.

---
+++ hellowebapp/settings_production.py
@@ -0,0 +1,11 @@
+from hellowebapp.settings import *
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUT = False
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
